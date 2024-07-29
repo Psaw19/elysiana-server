58dd7d75b9ed4c4f60faf32015bc790086f7b1a1
Prashant <psaw@test.in>
2024-07-29 05:32:38 +0000

    initial commit

---
+++ src/main/java/com/elysiana/ElysianaServerApplication.java
@@ -0,0 +1,13 @@
+package com.elysiana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElysianaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElysianaServerApplication.class, args);
+	}
+
+}
